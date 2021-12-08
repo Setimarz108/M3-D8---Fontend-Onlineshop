4fa4103ebdba45fc7c20826ce7ae64d19511dee1
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2021-12-08 21:18:54 +0100

    Task completed

---
+++ app.js
@@ -0,0 +1,51 @@
+
+        // const name = document.getElementById('name')
+        // const description = document.getElementById('Description')
+        // const brand = document.getElementById('brand')
+        // const price = document.getElementById('price')
+
+
+
+
+const getData = async () =>{
+
+    let getProductData = document.querySelector('.productInfo')
+    const url = "https://striveschool-api.herokuapp.com/api/product/"
+    
+    getProductData.addEventListener('submit', async (event) => {event.preventDefault()
+
+    let response= await fetch(url, { 
+      method: "POST",
+    headers: {
+       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjZlNjRjZmY1ZjAwMTU5MGJkYzciLCJpYXQiOjE2Mzg5NzExNjAsImV4cCI6MTY0MDE4MDc2MH0.3SKwQ-G9SnWsRu2xchG_dvAv8ZSkK4A6drgTORblK5E"
+      , 'Content-Type': 'application/json',
+    },
+
+    body : JSON.stringify({
+        name: `${event.target.name.value}`,
+        description: `${event.target.description.value}`,
+        brand: `${event.target.brand.value}`,
+        imageUrl: `${event.target.imageUrl.value}`,
+        price: `${event.target.price.value}`
+    })
+})
+     
+  let data = await response.json()
+  console.log(data)
+    
+    })
+   
+
+
+
+}
+
+window.onload = () => {
+    getData()
+}
+
+
+
+// console.log(response)
+
+
---
+++ productPage.js
@@ -0,0 +1,60 @@
+// fetching data
+
+const getProducts = async () => {
+
+    try{
+        const url = 'https://striveschool-api.herokuapp.com/api/product/'
+        let response = await fetch(url, {
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjZlNjRjZmY1ZjAwMTU5MGJkYzciLCJpYXQiOjE2Mzg5NzExNjAsImV4cCI6MTY0MDE4MDc2MH0.3SKwQ-G9SnWsRu2xchG_dvAv8ZSkK4A6drgTORblK5E"
+               }
+        })
+
+        let products = await response.json()
+        showProducts(products)
+        console.log(products)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+// DOM MANIPULATION _______DISPLAY NEW PRODUCTS
+
+const showProducts = (data) => {
+
+ let newItem = document.querySelector('#products')
+ console.log(newItem)
+
+ data.forEach(item => {
+
+    let addItem = document.createElement('div')
+    addItem.classList.add('col-sm-12', 'col-md-6', 'col-lg-3', 'mb-5', 'mx-2')
+
+    addItem.innerHTML = `
+    <div class="card border-info mb-3" style="width: 18rem; border-radius: 5%; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;">
+            <img src=${item.imageUrl} class="card-img-top" alt="..." style="width:100%;">
+            <div class="card-body">
+              <h5 class="card-title font-weight-bold">${item.name}</h5>
+              <p class="card-text">${item.description}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Brand : ${item.brand}</li>
+              <li class="list-group-item">Price : ${item.price}</li>
+            </ul>
+            <div class="card-body">
+                <a class='btn  mr-5' style="background-color:#59d9bf">Add to Cart</a>
+                <a class='btn btn-warning'>Buy</a>
+            </div>
+        </div>`
+
+        newItem.appendChild(addItem)
+     
+ });
+
+}
+
+window.onload = () => {
+getProducts()
+
+}
